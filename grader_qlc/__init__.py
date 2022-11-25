82423642b11e9413449ce577766c7b440409a289
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2022-11-25 20:17:33 +0200

    Fix console command

--- grader_qlc/__init__.py
+++ grader_qlc/__init__.py
@@ -1,1 +1,22 @@
+import subprocess
+import sys
+
 from files import read_output, rewrite_output
+
+def wrap():
+
+  # Execute normal grading in a separate process
+  if len(sys.argv) < 2:
+    print('qlc_wrap must preceed grading command on a line')
+    sys.exit(1)
+  subprocess.run(sys.argv[1:])
+
+  # Read the output from normal grading
+  output = read_output()
+
+  # Augment QLCs if full grade
+  if output['points'] >= output['max_points']:
+    
+    # TODO qlc data
+
+    rewrite_output('fi', output)
--- grader_qlc/__main__.py
+++ grader_qlc/__main__.py
@@ -1,20 +1,4 @@
-import subprocess
-import sys
+from . import wrap
 
-from .files import read_output, rewrite_output
-
-# Execute normal grading in a separate process
-if len(sys.argv) < 2:
-  print('qlc_wrap must preceed grading command on a line')
-  sys.exit(1)
-subprocess.run(sys.argv[1:])
-
-# Read the output from normal grading
-output = read_output()
-
-# Augment QLCs if full grade
-if output['points'] >= output['max_points']:
-  
-  # TODO qlc data
-
-  rewrite_output('fi', output)
+if __name__ == '__main__':
+  wrap()
